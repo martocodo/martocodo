dc98e404f96885671c94b8bd07e2ab32bc9bcc3b
Martin Lewis-Dalgarno <martin.lewis-dalgarno@infosum.com>
2020-02-17 13:29:59 +0000

    Initial build push

---
+++ site/build/precache-manifest.d8be6132274c39e09ebbcc1f62f4550b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd07be49096170a5e0d39275598899a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33b999f763a22af48868",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0d206b5b00d86697ea37",
+    "url": "/static/js/2.08d98e58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.08d98e58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b999f763a22af48868",
+    "url": "/static/js/main.3e775a55.chunk.js"
+  },
+  {
+    "revision": "9af90a3bdb44f1b1746a",
+    "url": "/static/js/runtime-main.fb18d836.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
